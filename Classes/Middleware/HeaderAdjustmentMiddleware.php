256fd6a087ef49d54824b8e0173ae74cf568d389
Martin Neundorfer <m.neundorfer@labor.digital>
2021-01-26 20:36:29 +0100

    feat(HeaderAdjustmentMiddleware): remove additional headers on both ways of the proxy

--- Classes/Middleware/HeaderAdjustmentMiddleware.php
+++ Classes/Middleware/HeaderAdjustmentMiddleware.php
@@ -30,19 +30,76 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 class HeaderAdjustmentMiddleware implements MiddlewareInterface
 {
+    public const FORBIDDEN_RESPONSE_HEADERS = [
+        'expect-ct',
+        'report-to',
+        'nel',
+        'vary',
+        'server',
+        'transfer-encoding',
+        'set-cookie'
+    ];
+
+    public const FORBIDDEN_RESPONSE_HEADER_PREFIXES = [
+        'cf-'
+    ];
 
     /**
      * @inheritDoc
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
+        $request = $this->removeUnwantedRequestHeaders($request);
         $request = $request->withHeader('x-webhub-external-proxy', 1);
 
         $response = $handler->handle($request);
 
-        $response = $response->withoutHeader('Transfer-Encoding');
+        return $this->removeUnwantedResponseHeaders($response);
+    }
+
+    /**
+     * Internal helper to remove unwanted information that should not be passed to the HuR infrastructure.
+     *
+     * @param   \Psr\Http\Message\ServerRequestInterface  $request
+     *
+     * @return \Psr\Http\Message\ServerRequestInterface
+     */
+    protected function removeUnwantedRequestHeaders(ServerRequestInterface $request): ServerRequestInterface {
+
+        // Remove all cookies, so we never get any information we should not get
+        $request = $request->withCookieParams([]);
+        $request = $request->withoutHeader('cookie');
+
+        return $request;
+    }
+
+    /**
+     * Internal helper to filter out all privacy relevant headers we don't want to transfer to the real user
+     *
+     * @param   \Psr\Http\Message\ResponseInterface  $response
+     *
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    protected function removeUnwantedResponseHeaders(ResponseInterface $response): ResponseInterface{
+        $headers = $response->getHeaders();
+
+        foreach ($headers as $name => $content){
+            $lcName = strtolower($name);
+
+            if(in_array($lcName, static::FORBIDDEN_RESPONSE_HEADERS, true)){
+                $response = $response->withoutHeader($name);
+                continue;
+            }
+
+            foreach (static::FORBIDDEN_RESPONSE_HEADER_PREFIXES as $prefix){
+                if(strpos($lcName, $prefix) === 0) {
+                    $response = $response->withoutHeader($name);
+                    continue 2;
+                }
+            }
+
+        }
 
         return $response;
     }
-
 }
